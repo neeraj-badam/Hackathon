4c226a3c465a4e8d618aa88dfb936445252989ee
neeraj-badam <neeraj.badam@gmail.com>
2025-02-15 11:14:58 -0500

    Added Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,10 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const Product = require('../models/Product');
+const { getProducts, addProduct } = require('../controllers/productController');
+const authMiddleware = require('../middleware/authMiddleware').authMiddleware;
+
+const router = express.Router();
+
+router.get('/', getProducts);
+router.post('/add', authMiddleware, addProduct);
+
+module.exports = router;
