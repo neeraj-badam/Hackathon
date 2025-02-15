a378dcc19c4fbd76b326033ac098dbea18532edf
neeraj-badam <neeraj.badam@gmail.com>
2025-02-15 09:56:39 -0500

    Added authRoutes.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.registerUser = async (req, res) => {
+  console.log( req.body );
+  const { name, email, password, phone, address } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password: hashedPassword, phone, address, role: 'user' });
+
+  try {
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(400).json({ error: 'Error creating user' });
+  }
+}
+
+exports.loginUser = async (req, res) => {
+  console.log( req.body );
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    res.json({ token, user });
+  } else {
+    res.status(401).json({ error: 'Invalid credentials' });
+  }
+}
