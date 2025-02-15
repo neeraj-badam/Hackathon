8eeaba9b2499c88dbc62971047d24c5c93e57cec
VasaGanesh <ganeshvasa72@gmail.com>
2025-02-15 09:21:57 -0500

    Added Order Schema

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require('jsonwebtoken');
 
+const Order = require("./models/Order");
 const Product = require("./models/Product");
 const User = require("./models/User");
 
